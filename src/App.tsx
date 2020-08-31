a663bab0e3b51f1c1a468c2bfa4c9ab5a6e01465
Patrick Jean Meurer <potreco@live.com>
2020-08-31 09:59:50 -0300

    Logout e Pasta de Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 // Pages
 import SignIn from './pages/SignIn';
